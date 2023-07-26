854b4e62561cb0af961d9ef552f4980edf0dd2fe
Diana Man <madi22gc@student.ju.se>
2023-07-26 14:20:11 +0300

    update

---
+++ script.js
@@ -0,0 +1,19 @@
+let hotAirBalloon;
+
+
+function preload(){
+    hotAirBalloon = createImg('air-balloon-png-transparent-background-9.png');
+}
+
+function setup(){
+    createCanvas(400, 400);
+}
+
+function draw(){
+background(230);
+hotAirBalloon.position(100, 100);
+}
+
+
+
+
