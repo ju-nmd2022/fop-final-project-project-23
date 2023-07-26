466007953162f15bca505be6a237f1eb3ae3540e
Diana Man <madi22gc@student.ju.se>
2023-07-26 15:49:35 +0300

    update

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let balloon;
-
+let cloud;
+let posX=0;
+let posY=0;
 
 function preload(){
     balloon = loadImage("balloon.png");
+    cloud = loadImage("cloud.png");
+    cloud = loadImage("cloud.png");
+    cloud = loadImage("cloud.png");
 }
 
 function setup(){
@@ -11,7 +16,15 @@ function setup(){
 
 function draw(){
 background(230);
-image(balloon, 0, 250, 100, 100);
+image(balloon, posX, 250, 100, 100);
+image(cloud, posY+100, 0, 250, 150);
+image(cloud, posY+400, 50, 300, 150);
+image(cloud, posY+700, 0, 350, 170);
+
+
+posX+=1;
+posY-=1;
+
 }
 
 
