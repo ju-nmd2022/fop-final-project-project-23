c2fc4658d7dba922d9ca06fa27ab3872bad7ffe0
Diana Man <madi22gc@student.ju.se>
2023-07-31 15:04:13 +0300

    update

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let balloonY = 300;
 let balloonX =0;
 let isLeft;
 let isRight;
+let isUp;
+let isDown;
 
 
 function preload(){
@@ -21,7 +23,7 @@ function preload(){
 }
 
 function setup(){
-    createCanvas(800, 800);
+    createCanvas(700, 700);
     scroll=0;
 
     isRight= false;
@@ -40,11 +42,11 @@ image(cloud, posY+ posX + 200, 50, 300, 150);
 image(cloud, posY+400, posX + 0, 350, 170);
 image(cloud, posY+600, posX + 70, 350, 170);
 image(cloud, posY+100, posX + 100, 250, 190);
-image(grass, posY, posY+650, 850, 100);
+image(grass, posY, posY+600, 850, 100);
 
 
 if(isLeft){
-    if(balloonX > width * 0.01){
+    if(balloonX < width ){
         balloonX-=5;
     }
     else{
@@ -54,8 +56,28 @@ if(isLeft){
 }
 
 if(isRight){
-    if(balloonX < width * 1){
+    if(balloonX < width ){
         balloonX+=5;
+    }
+    else{
+        scroll -=5;
+    }
+
+}
+
+if(isUp){
+    if(balloonY < width ){
+        balloonY-=5;
+    }
+    else{
+        scroll +=5;
+    }
+
+}
+
+if(isDown){
+    if(balloonY < width ){
+        balloonY+=5;
     }
     else{
         scroll -=5;
@@ -73,6 +95,14 @@ function keyPressed(){
     if(keyCode == RIGHT_ARROW){
         isRight=true;
     }
+
+    if(keyCode == UP_ARROW){
+        isUp=true;
+    }
+
+    if(keyCode == DOWN_ARROW){
+        isDown = true;
+    }
 }
 
 
@@ -82,6 +112,14 @@ function keyReleased(){
     }
     if(keyCode == RIGHT_ARROW){
         isRight=false;
+    }
+
+    if(keyCode == UP_ARROW){
+        isUp=false;
+    }
+
+    if(keyCode == DOWN_ARROW){
+        isDown = false;
     }
 }
 
